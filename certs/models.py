fad5774f3920c12dad2cfea2fb640f3c6d156269
Jeremy Price <github@jerm.org>
2016-10-02 22:15:21 -0700

    stating to model the models

--- certs/models.py
+++ certs/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Cert(models.Model):
+    owner = models.ForeignKey('accounts.', related_name='certs',
+                    null=True, blank=True)
+    cn = models.CharField(max_length=255, null=False, blank=False)
+    fingerprint models.CharField(max_length=255, null=True, blank=True)
+    create_datetime
+    last_check_datetime
+    
 # Create your models here.
